9b24d511c42e9b22fa581d2bafb079652dc89b98
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-11-21 09:03:40 -0600

    gets the JS to write the HTML elements

--- docs/index_files/gallerywidget-binding-0.0.0.9000/gallerywidget.js
+++ docs/index_files/gallerywidget-binding-0.0.0.9000/gallerywidget.js
@@ -9,19 +9,37 @@ HTMLWidgets.widget({
     return {
       renderValue: function(x) {
 
+        // function to create style-string
+        var makeStyle = function(x) {
+          return 'background-image: url("' + x + '");';
+        };
+
+        // convert data to row-major format
         var data = HTMLWidgets.dataframeToD3(x.data);
-        console.log(data);
 
         // for each row in x, add an <a> element
         for (var i = 0; i < data.length; i++) {
 
           var row = data[i];
-          var a = document.createElement("a");
 
-          a.setAttribute("href", row.href);
+          var preview = document.createElement("a");
+          preview.className = "gallerywidget-preview";
+          preview.setAttribute("href", row.href);
+          preview.setAttribute("style", makeStyle(row.image));
 
-          console.log(a);
-          el.appendChild(a);
+          var title = document.createElement("div");
+          title.className = "gallerywidget-title";
+          title.textContent = row.title;
+
+          var subtitle = document.createElement("div");
+          subtitle.className = "gallerywidget-subtitle";
+          subtitle.textContent = row.subtitle;
+
+          preview.appendChild(title);
+          preview.appendChild(subtitle);
+
+          console.log(preview);
+          el.appendChild(preview);
         }
 
       },
--- inst/htmlwidgets/gallerywidget.js
+++ inst/htmlwidgets/gallerywidget.js
@@ -9,19 +9,42 @@ HTMLWidgets.widget({
     return {
       renderValue: function(x) {
 
+        // function to create style-string
+        var makeStyle = function(x) {
+          return 'background-image: url("' + x + '");';
+        };
+
+        // convert data to row-major format
         var data = HTMLWidgets.dataframeToD3(x.data);
-        console.log(data);
 
         // for each row in x, add an <a> element
         for (var i = 0; i < data.length; i++) {
 
           var row = data[i];
-          var a = document.createElement("a");
 
-          a.setAttribute("href", row.href);
+          // create preview element
+          var preview = document.createElement("a");
+          preview.className = "gallerywidget-preview";
+          preview.setAttribute("href", row.href);
+          preview.setAttribute("style", makeStyle(row.image));
 
-          console.log(a);
-          el.appendChild(a);
+          // create title, subtitle elements
+          var title = document.createElement("div");
+          title.className = "gallerywidget-title";
+          title.textContent = row.title;
+
+          var subtitle = document.createElement("div");
+          subtitle.className = "gallerywidget-subtitle";
+          subtitle.textContent = row.subtitle;
+
+          // append title, subtitle to preview
+          preview.appendChild(title);
+          preview.appendChild(subtitle);
+
+          // append preview to enclosing element
+          el.appendChild(preview);
+
+          //console.log(preview);
         }
 
       },
